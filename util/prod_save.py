6b461a9bac2a125b651a0eb73b24b413052b6a9d
Ricardo de Carvalho <ricardaodopao952@gmail.com>
2026-08-11 20:05:56 -0300

    Add files via upload

--- util/prod_save.py
+++ util/prod_save.py
@@ -1,5 +1,9 @@
+# prod_save.py
+
 import json
 import os
+
+from util.sessao import obter_usuario
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PASTA_DADOS = os.path.join(BASE_DIR, "dados")
@@ -20,6 +24,18 @@ def carregar_produtos():
     except (json.JSONDecodeError, Exception):
         return []
 
+def carregar_meus_produtos():
+
+    usuario = obter_usuario()
+
+    produtos = carregar_produtos()
+
+    return [
+        produto
+        for produto in produtos
+        if produto.get("usuario") == usuario
+    ]
+
 def salvar_produtos(produtos):
     if not os.path.exists(PASTA_DADOS):
         os.makedirs(PASTA_DADOS)
@@ -28,8 +44,11 @@ def salvar_produtos(produtos):
         json.dump(produtos, f, indent=4, ensure_ascii=False)
 
 def produto_existe(nome_produto):
+    usuario_atual = obter_usuario()
+
     produtos = carregar_produtos()
     return any(
+        p["usuario"] == usuario_atual and
         p.get("nome_produto", "").lower() == nome_produto.lower()
         for p in produtos
     )
@@ -38,9 +57,12 @@ def criar_produto(nome_produto, preco, quantidade):
     if produto_existe(nome_produto):
         return False
 
+    usuario_atual = obter_usuario()
+
     produtos = carregar_produtos()
     produtos.append({
         "nome_produto": nome_produto,
+        "usuario": usuario_atual,
         "preco": preco,
         "quantidade": quantidade
     })
